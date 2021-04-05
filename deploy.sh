cb9d9b960597303f9122b0bef575afabd0560afe
Achmad Chun-Chun Winata Adi <anonychun@gmail.com>
2021-04-06 00:14:01 +0700

    rebuilding site Tue Apr  6 12:13:45 AM WIB 2021

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+rm -rf public
+git submodule add -b master --force git@github.com:struggleapparel/struggleapparel.github.io.git public
+cd public
+shopt -s extglob
+rm -r !(".git")
+cd ..
+
+hugo
+
+cd public
+git add -A
+git commit -m "$msg"
+git push origin master
+
+cd ..
+git add -A
+git commit -m "$msg"
+git push origin master
